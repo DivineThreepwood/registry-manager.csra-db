2207f48afb91d225b3bc10467d42af975440c374
Marian Pohling <mpohling@cit-ec.uni-bielefeld.de>
2016-09-16 16:56:39 +0200

    fix reset scipt which was just executing all reset operations for the db root folder instead of each submodules.

--- reset.sh
+++ reset.sh
@@ -4,8 +4,6 @@ echo "reset local dbs..."
 git reset --hard
 git clean -f
 
+# make sure all submodules are initialized and updated.
 ./init_update.sh
-./device-class-db/reset.sh
-./unit-template-db/reset.sh
-./app-class-db/reset.sh
-./agent-class-db/reset.sh
+git submodule foreach ./reset.sh
